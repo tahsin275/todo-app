aea7b2ebe1ce0cb3aca8dee1bed3fbdb4c737742
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2022-01-24 04:52:53 +0600

    drag and drop feature, code refactored

--- script/app.js
+++ script/app.js
@@ -1,50 +1,44 @@
-// console.log('working');
-
 // Caching the DOM Elements
-const userInput = document.querySelector('.todo-input');
-const button = document.querySelector('.todo-button');
-const list = document.querySelector('.todo-list');
+const userInput = document.querySelector(".todo-input");
+const button = document.querySelector(".todo-button");
+const list = document.querySelector(".todo-list");
 
-
-// TODO LIST
-//  1. Capturing the user input
-//  2. Create all the elements
-//  3. Insert class and text input
-//  4. Append child elements to parent element
-
-button.addEventListener('click', function(event){
+button.addEventListener("click", function (event) {
     event.preventDefault();
     const input = userInput.value;
-    const div = document.createElement('div');
-    div.className = 'todo';
-    const li = document.createElement('li');
-    li.className = 'todo-item';
-    li.textContent = input;
-    div.appendChild(li);
-    const checkBtn = document.createElement('button');
-    checkBtn.className = 'check';
-    checkBtn.innerHTML = '<i class="fas fa-check"></i>';
-    div.appendChild(checkBtn);
-    const trashBtn = document.createElement('button');
-    trashBtn.className = 'trash';
-    trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
-    div.appendChild(trashBtn);
-    list.appendChild(div);
-    userInput.value = '';
-})
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <p class="todo-item">
+            <span><i class="fas fa-bars"></i></span> ${input}
+        </p>
+        <div class="butons">
+            <button class="check">
+                <i class="fas fa-check"></i>
+            </button>
+            <button class="trash">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>
+        `;
+    div.className = "todo";
+    list.append(div);
+    userInput.value = "";
+});
 
-list.addEventListener('click',function(event){
+list.addEventListener("click", function (event) {
     const clickedItem = event.target;
-    if(clickedItem.className == 'check'){
-        const div = clickedItem.parentNode;
-        div.classList.add('completed');
+    if (clickedItem.className == "check") {
+        const div = clickedItem.parentNode.parentNode;
+        div.classList.add("completed");
         clickedItem.remove();
-    } else if(clickedItem.className == 'trash'){
-        const div = clickedItem.parentNode;
-        div.classList.add('drop-effect');
-        div.addEventListener('transitionend', function(){
+    } else if (clickedItem.className == "trash") {
+        const div = clickedItem.parentNode.parentNode;
+        div.classList.add("drop-effect");
+        div.addEventListener("transitionend", function () {
             div.remove();
-            console.log('event occured')
-        })   
+            console.log("event occured");
+        });
     }
-})
+});
+
+// list.addEventListener("d");
